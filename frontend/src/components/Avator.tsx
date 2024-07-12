01ca90ac32e92252c16b39f882427a7807a8da2c
uttam282005 <uttam282005@gmail.com>
2024-07-12 12:10:11 +0530

    solved build issue

--- frontend/src/components/Avator.tsx
+++ frontend/src/components/Avator.tsx
@@ -1,14 +1,5 @@
-const colorClasses: { [key: string]: string } = {
-  red: 'bg-red-600',
-  blue: 'bg-blue-600',
-  green: 'bg-green-600',
-  // Add more colors as needed
-  slate: 'bg-slate-600',
-};
-
-export const Avator = ({ name, color, size }: { name: string, color?: string, size?: "sm" | "md" }) => {
-  color = colorClasses[color] || colorClasses.red;
-  return <div className={`rounded-full ${size == "sm" ? "h-5 w-5" : "h-8 w-8"} text-xs place-content-center text-center uppercase text-white ${color}`}>
+export const Avator = ({ name, size }: { name: string, size?: "sm" | "md" }) => {
+  return <div className={`rounded-full ${size == "sm" ? "h-5 w-5" : "h-8 w-8"} text-xs place-content-center text-center uppercase text-white bg-red-600`}>
     {name[0]}
   </div >
 }
