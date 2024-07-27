2da1166545f65c579d7ae717fa374efeee7ddf3a
uttam282005 <uttam282005@gmail.com>
2024-07-28 02:24:56 +0530

    added syntax highlighting

--- frontend/src/pages/Preview.tsx
+++ frontend/src/pages/Preview.tsx
@@ -1,14 +1,14 @@
-import { MDXRemote } from 'next-mdx-remote';
-import { useSerialize } from '../hooks';
+import { ShowMDX } from "../components/ShowMDX";
 
 export const Preview = ({ source }: { source: string }) => {
-  const mdxSource = useSerialize(source)
-  return mdxSource !== null ? <div className='border border-10'>
-    <div className='font text-slate-700 px-2 py-2'>
-      Mdx Preview
+
+  return (
+    <div className="border border-10">
+      <div className="font text-slate-700 px-2 py-2">Mdx Preview</div>
+      <div className="px-2 py-2 max-h-[336px] overflow-auto" >
+        <ShowMDX source={source} />
+      </div>
     </div>
-    <div className='px-2 py-2'>
-      <MDXRemote {...mdxSource} />
-    </div>
-  </div> : <div> Compilation failed </div>
-}
+  )
+};
+
